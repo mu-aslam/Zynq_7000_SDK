974d5788106ff2a9d687326ee070ada7e9fefba5
aslaamshaafi <aslaam.shafee@gmail.com>
2019-01-05 22:32:21 +0100

    uart 0 & 1 C drivers

---
+++ project_1.sdk/test_uart/Debug/src/helloworld.d
@@ -0,0 +1,48 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../test_uart_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../test_uart_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../test_uart_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../test_uart_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../test_uart_bsp/ps7_cortexa9_0/include/xuartps.h \
+ ../../test_uart_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../test_uart_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../test_uart_bsp/ps7_cortexa9_0/include/xuartps_hw.h \
+ ../../test_uart_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../test_uart_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../test_uart_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../test_uart_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../test_uart_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../test_uart_bsp/ps7_cortexa9_0/include/xplatform_info.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../test_uart_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../test_uart_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../test_uart_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../test_uart_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../test_uart_bsp/ps7_cortexa9_0/include/xuartps.h:
+
+../../test_uart_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../test_uart_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../test_uart_bsp/ps7_cortexa9_0/include/xuartps_hw.h:
+
+../../test_uart_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../test_uart_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../test_uart_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../test_uart_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../test_uart_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../test_uart_bsp/ps7_cortexa9_0/include/xplatform_info.h:
